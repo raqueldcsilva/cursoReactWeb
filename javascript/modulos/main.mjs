aba695349978fd6531a6bef344b5ad8bb7e2d325
Raqueldcsilva <raquelshdy@gmail.com>
2021-11-05 22:20:39 -0300

    exercícios de JS

---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Raquel', 23))
+console.log(mostraCidade('Raquel', 'São Paulo'))
+console.log(mostraHobby('Raquel', 'ler livros'))
